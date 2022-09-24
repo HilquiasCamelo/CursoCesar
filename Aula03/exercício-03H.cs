b5b9566735b8c44964f467509d47a1aae92ba71d
“HilquiasCamelo” <hilquiaswpc5@gmail.com>
2022-09-24 15:23:48 -0300

    Update exercício-03H.cs

--- Aula03/exercício-03H.cs
+++ Aula03/exercício-03H.cs
@@ -1,28 +1,68 @@
 // Leia três números quaisquer, imprimindo-os em ordem crescente.
 
 using System;
-using System.Diagnostics.Metrics;
+using System.Collections.Generic;
 
-internal class Program
+namespace _3H
 {
-
-    private static void Main(string[] args)
-
+    class Program
     {
-        Console.WriteLine("Digite 3 números aleatoriamente.: ");
-        int[] vetor = new int[3];
-      
-        for (int i = 0; i < 3; i++)
+        static void Main(string[] args)
         {
-            Console.Write("Digite as notas do aluno .: ");
-            vetor[i] = int.Parse(Console.ReadLine());
-        }
-        Array.Sort(vetor);
+            List<char> listaRespostas = new List<char>();
 
-        foreach (int valor in vetor)
-        {
-            Console.Write(" ");
-            Console.Write(valor + " ");
+            char resposta = ' ';
+            while (resposta != '-')
+            {
+                Console.Clear();
+                Console.WriteLine(" O  Ótimo");
+                Console.WriteLine(" B  Bom");
+                Console.WriteLine(" R  Regular");
+                Console.WriteLine(" U  Ruim");
+                Console.WriteLine(" -  Sair");
+                Console.WriteLine();
+                Console.Write(" Opção: ");
+                resposta = char.Parse(Console.ReadLine().ToUpper());
+                if (resposta == 'O' || resposta == 'B' || resposta == 'R' || resposta == 'U')
+                    listaRespostas.Add(resposta);
+            }
+
+            int bom = 0, otimo = 0, regular = 0, ruim = 0, total = 0;
+            foreach (char respostaAtual in listaRespostas)
+            {
+                total++;
+                switch (respostaAtual)
+                {
+                    case 'B':
+                        bom++;
+                        break;
+                    case 'O':
+                        otimo++;
+                        break;
+                    case 'R':
+                        regular++;
+                        break;
+                    case 'U':
+                        ruim++;
+                        break;
+                }
+            }
+            Console.WriteLine($"Total de respostas = {total}");
+            Console.WriteLine($"==================================");
+            Console.WriteLine($"Ótimo teve....{otimo}... respostas");
+            Console.WriteLine($"Bom teve......{bom}..... respostas");
+            Console.WriteLine($"Regular teve..{regular}. respostas");
+            Console.WriteLine($"Ruim teve.....{ruim}..... respostas");
+            Console.WriteLine($"==================================");
+            Console.WriteLine($"Ótimo teve....{(((float)otimo / (float)total) * 100).ToString("F1")}%... respostas");
+            Console.WriteLine($"Bom teve......{(((float)bom / (float)total) * 100).ToString("F1")}%..... respostas");
+            Console.WriteLine($"Regular teve..{(((float)regular / (float)total) * 100).ToString("F1")}%. respostas");
+            Console.WriteLine($"Ruim teve.....{(((float)ruim / (float)total) * 100).ToString("F1")}%..... respostas");
+            Console.WriteLine($"==================================");
+            int soma_ob = otimo + bom;
+            int soma_ru = regular + ruim;
+            if (soma_ob > soma_ru)
+                Console.WriteLine($"Soma de Otimos e bons ({soma_ob}) é maior que a soma dos regulares e ruins({soma_ru})");
         }
     }
 }
